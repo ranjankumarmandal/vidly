d89a26d0d0894520168f6ba541695530db77431c
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-09-14 10:02:54 +0530

    list group v2

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -55,7 +55,7 @@ class Movies extends Component {
 
     return (
       <div className="row">
-        <div className="col-2">
+        <div className="col-3">
           <ListGroup
             items={this.state.genres}
             onClick={this.handleGenereSelect}
